84ffc3a7c427bb9e35df7717ebd8e4b35739ec67
heinanX <larrii@live.se>
2022-11-28 10:36:55 +0100

    Added constuctor and getters to product.php

--- Product.php
+++ Product.php
@@ -8,6 +8,26 @@ class Product
     private $price;
     private $inStock;
 
+    function __construct($id, $title, $price, $inStock) {
+        $this->id = $id; 
+        $this->title = $title; 
+        $this->price = $price; 
+        $this->inStock = $inStock; 
+    }
+
+      function getId() {
+        return $this->id;
+      }
+      function getTitle() {
+        return $this->title;
+      }
+      function getPrice() {
+        return $this->price;
+      }
+      function getInStock() {
+        return $this->inStock;
+    }
+
     // TODO Skriv en konstruktor som sätter alla properties
 
     // TODO Skriv getters för alla properties
